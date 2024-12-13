0150067095b7cc3d93c8cbcffee2a93940bbc445
vachan-maker <vachan@tutanota.com>
2024-12-13 13:59:19 +0530

    Drag start event

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const infoDisplay = document.querySelector("#info")
 //added an event listener, dragging function is executed when the event occurs
 king.addEventListener("drag", dragging)
 
+king.addEventListener("dragstart", dragStart)
+
 function dragging (e) {
     console.log(e.target + "is being dragged.")
+}
+
+function dragStart (e) {
+    console.log("Dragging has started on" + e.target)
+
 }
